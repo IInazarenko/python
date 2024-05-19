2e1ca750628bad4b66685fc6129e937105168487
IINazarenko <dragon567876@gmail.com>
2024-05-19 11:05:56 +0300

    hw8  working!!!

--- hw8/main.py
+++ hw8/main.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     L = sp.symbols('L')
     m = sp.symbols('m')
     M = sp.symbols('M')
-    u1 = sp.symbols('u1')
-    u2 = sp.symbols('u2')
+    u1 = sp.symbols('u1', cls=sp.Function)(t)
+    u2 = sp.symbols('u2', cls=sp.Function)(t)
 
     H01 = sp.Matrix([[sp.cos(sp.pi/2), -sp.sin(sp.pi/2), 0, 0], [sp.sin(sp.pi/2), sp.cos(sp.pi/2), 0, 0],
                      [0, 0, 1, 0], [0, 0, 0, 1]]) @\
@@ -34,14 +34,18 @@ if __name__ == '__main__':
     H = H01 @ H12 @ H23 @ H34
 
     rm = H @ sp.Matrix([[0], [0], [0], [1]])
+    print(rm)
     # print(H)
 
     # print(rm)
+    print(rm[0, 0])
+    print(rm[1, 0])
+    print(rm[2, 0])
 
-    v = sp.diff(rm[0, 0], t)**2 + sp.diff(rm[1, 0], t)**2 + sp.diff(rm[2, 0], t)**2 + sp.diff(rm[3, 0], t)**2
+    v = sp.diff(rm[0, 0], t)**2 + sp.diff(rm[1, 0], t)**2 + sp.diff(rm[2, 0], t)**2
     # print (v.simplify().expand())
 
-    P = m * g * L * sp.cos(e2)
+    P = m * g * rm[1, 0]
 
     KM = 1 / 2 * M * (sp.diff(x1, t)**2 + sp.diff(x2, t)**2)
     Km = 1 / 2 * m * v;
@@ -51,6 +55,7 @@ if __name__ == '__main__':
 
     dL_dx1 = sp.diff(L, x1)
     x1dot = sp.diff(x1, t)
+    x1dotdot = sp.diff(x1, t, 2)
     dL_dX1dot = sp.diff(L, x1dot)
     d_dt_dLd_dX1dot = sp.diff(dL_dX1dot, t)
     Lagrangian_x1 = d_dt_dLd_dX1dot - dL_dx1
@@ -59,6 +64,7 @@ if __name__ == '__main__':
 
     dL_de1 = sp.diff(L, e1)
     e1dot = sp.diff(e1, t)
+    e1dotdot = sp.diff(e1, t, 2)
     dL_de1dot = sp.diff(L, e1dot)
     d_dt_dLd_de1dot = sp.diff(dL_de1dot, t)
     Lagrangian_e1 = d_dt_dLd_de1dot - dL_de1
@@ -67,6 +73,7 @@ if __name__ == '__main__':
 
     dL_dx2 = sp.diff(L, x2)
     x2dot = sp.diff(x2, t)
+    x2dotdot = sp.diff(x2, t, 2)
     dL_dX2dot = sp.diff(L, x2dot)
     d_dt_dLd_dX2dot = sp.diff(dL_dX2dot, t)
     Lagrangian_x2 = d_dt_dLd_dX2dot - dL_dx2
@@ -75,26 +82,49 @@ if __name__ == '__main__':
 
     dL_de2 = sp.diff(L, e2)
     e2dot = sp.diff(e2, t)
+    e2dotdot = sp.diff(e2, t, 2)
     dL_de2dot = sp.diff(L, e2dot)
     d_dt_dLd_de2dot = sp.diff(dL_de2dot, t)
     Lagrangian_e2 = d_dt_dLd_de2dot - dL_de2
     print("e2 ")
     print(Lagrangian_e2.simplify().expand())
 
-    leq1 = Lagrangian_x1.expand().simplify()
-    leq2 = Lagrangian_x2.expand().simplify()
-    leq3 = Lagrangian_e1.expand().simplify()
-    leq4 = Lagrangian_e2.expand().simplify()
+    leq1 = Lagrangian_x1 - u1
+    leq2 = Lagrangian_x2 - u2
+    leq3 = Lagrangian_e1
+    leq4 = Lagrangian_e2
 
-    system_of_equations = [sp.Eq(leq1, 0), sp.Eq(leq2, 0), sp.Eq(leq3, 0), sp.Eq(leq4, 0)]
-    solutions = sp.solve(system_of_equations, (sp.diff(e1, t, 2), sp.diff(e2, t, 2) ,sp.diff(x1, t, 2), sp.diff(x2, t, 2)))
+    print("leq1")
+    sp.print_latex(leq1.simplify())
+    print("leq2")
+    sp.print_latex(leq2.simplify())
+    print("leq3")
+    sp.print_latex(leq3.simplify())
+    print("leq4")
+    sp.print_latex(leq4.simplify())
 
-    print(solutions.keys())
+    MatrixA = sp.Matrix([[sp.diff(leq1, x1dotdot), sp.diff(leq1, x2dotdot), sp.diff(leq1, e1dotdot), sp.diff(leq1, e2dotdot)],
+                         [sp.diff(leq2, x1dotdot), sp.diff(leq2, x2dotdot), sp.diff(leq2, e1dotdot), sp.diff(leq2, e2dotdot)],
+                         [sp.diff(leq3, x1dotdot), sp.diff(leq3, x2dotdot), sp.diff(leq3, e1dotdot), sp.diff(leq3, e2dotdot)],
+                         [sp.diff(leq4, x1dotdot), sp.diff(leq4, x2dotdot), sp.diff(leq4, e1dotdot), sp.diff(leq4, e2dotdot)]])
 
-    e1dotdot = solutions[list(solutions.keys())[0]]
-    e2dotdot = solutions[list(solutions.keys())[1]]
-    x1dotdot = solutions[list(solutions.keys())[2]]
-    x2dotdot = solutions[list(solutions.keys())[3]]
+    print("matrixA")
+    sp.print_latex(MatrixA)
+    MatrixB = sp.Matrix([[-(leq1 - sp.diff(leq1, x1dotdot)*x1dotdot - sp.diff(leq1, x2dotdot)*x2dotdot - sp.diff(leq1, e1dotdot)*e1dotdot - sp.diff(leq1, e2dotdot)*e2dotdot)],
+                         [-(leq2 - sp.diff(leq2, x1dotdot)*x1dotdot - sp.diff(leq2, x2dotdot)*x2dotdot - sp.diff(leq2, e1dotdot)*e1dotdot - sp.diff(leq2, e2dotdot)*e2dotdot)],
+                         [-(leq3 - sp.diff(leq3, x1dotdot)*x1dotdot - sp.diff(leq3, x2dotdot)*x2dotdot - sp.diff(leq3, e1dotdot)*e1dotdot - sp.diff(leq3, e2dotdot)*e2dotdot)],
+                         [-(leq4 - sp.diff(leq4, x1dotdot)*x1dotdot - sp.diff(leq4, x2dotdot)*x2dotdot - sp.diff(leq4, e1dotdot)*e1dotdot - sp.diff(leq4, e2dotdot)*e2dotdot)]])
+    print("matrixB")
+    for item in MatrixB:
+        print(" ")
+        sp.print_latex(item.simplify())
+    MatrixX = MatrixA.inv()@MatrixB
+    print("X")
+    print(MatrixX)
+    x1dotdot = MatrixX[0, 0]
+    x2dotdot = MatrixX[1, 0]
+    e1dotdot = MatrixX[2, 0]
+    e2dotdot = MatrixX[3, 0]
 
     a51 = sp.diff(x1dotdot, x1)
     a52 = sp.diff(x1dotdot, x2)
@@ -142,9 +172,16 @@ if __name__ == '__main__':
 
     A = sp.Matrix([[0,0,0,0,1,0,0,0], [0,0,0,0,0,1,0,0], [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,1],
                    [a51, a52, a53, a54, a55, a56, a57, a58], [a61, a62, a63, a64, a65, a66, a67, a68],
-                   [a71, a72, a73, a74, a75, a76, a77, a78], [a81, a82, a83, a84, a85, a86, a87, a88]]).subs({x1: 0, x2: 0, e1: 0, e2: 0,
+                   [a71, a72, a73, a74, a75, a76, a77, a78], [a81, a82, a83, a84, a85, a86, a87, a88]]).subs({x1: 0, x2: 0, e1: sp.pi/2, e2: 0,
                                                                                                               x1dot: 0, x2dot: 0, e1dot: 0, e2dot: 0})
-    B = sp.Matrix([[0, 0], [0, 0], [0, 0], [0, 0], [u51, u52], [u61, u62], [u71, u72], [u81, u82]]).subs({x1: 0, x2: 0, e1: 0, e2: 0,
+    B = sp.Matrix([[0, 0], [0, 0], [0, 0], [0, 0], [u51, u52], [u61, u62], [u71, u72], [u81, u82]]).subs({x1: 0, x2: 0, e1: sp.pi/2, e2: 0,
                                                                                                               x1dot: 0, x2dot: 0, e1dot: 0, e2dot: 0})
+    print("A")
     sp.print_latex(A)
+    print("B")
     sp.print_latex(B)
+
+    control_mat = B.row_join(A @ B).row_join(A @ A @ B).row_join(A @ A @ A @ B).row_join(A @ A @ A @ A @ B).row_join(A @ A @ A @ A @ A @ B).row_join(A @ A @ A @ A @ A @ A @ B).row_join(A @ A @ A @ A @ A @ A @ A @ B)
+    print("control ")
+    sp.print_latex(control_mat)
+    print(control_mat.rank())
